e8872daf07b6b21f6ef95aa4e7323f41f7816377
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-04-25 22:11:52 -0300

    Insert arrows

--- src/app/food-selection/food-selection.component.ts
+++ src/app/food-selection/food-selection.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./food-selection.component.scss"],
 })
 export class FoodSelectionComponent implements OnInit {
+  public expandOff = false;
+  public expandHouse = false;
+  public expandExtra = false;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  expand(event: string) {
+    if (event === "off") {
+      this.expandOff = !this.expandOff;
+    }
+    if (event === "house") {
+      this.expandHouse = !this.expandOff;
+    }
+    if (event === "extra") {
+      this.expandExtra = !this.expandExtra;
+    }
+  }
 }
