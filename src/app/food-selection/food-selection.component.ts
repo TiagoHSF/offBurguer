5a3a71201f61599728963c3ffec285af9fc8184c
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2022-04-25 22:50:53 -0300

    ajustes main page

--- src/app/food-selection/food-selection.component.ts
+++ src/app/food-selection/food-selection.component.ts
@@ -6,9 +6,16 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./food-selection.component.scss"],
 })
 export class FoodSelectionComponent implements OnInit {
+  isCheckedLanche = false;
+  isCheckedPizza = false;
+
   public expandOff = false;
   public expandHouse = false;
   public expandExtra = false;
+
+  public expandOffPizza = false;
+  public expandQueijoPizza = false;
+  public expandBadassPizza = false;
 
   constructor() {}
 
@@ -19,10 +26,37 @@ export class FoodSelectionComponent implements OnInit {
       this.expandOff = !this.expandOff;
     }
     if (event === "house") {
-      this.expandHouse = !this.expandOff;
+      this.expandHouse = !this.expandHouse;
     }
     if (event === "extra") {
       this.expandExtra = !this.expandExtra;
+    }
+  }
+
+  expandPizza(event: string) {
+    if (event === "off") {
+      this.expandOffPizza = !this.expandOffPizza;
+    }
+    if (event === "queijo") {
+      this.expandQueijoPizza = !this.expandQueijoPizza;
+    }
+    if (event === "badass") {
+      this.expandBadassPizza = !this.expandBadassPizza;
+    }
+  }
+
+  check(event: string) {
+    if (event === "lanche") {
+      this.isCheckedLanche = !this.isCheckedLanche;
+      this.expandOff = false;
+      this.expandHouse = false;
+      this.expandExtra = false;
+    }
+    if (event === "pizza") {
+      this.isCheckedPizza = !this.isCheckedPizza;
+      this.expandOffPizza = false;
+      this.expandQueijoPizza = false;
+      this.expandBadassPizza = false;
     }
   }
 }
